85ccfb62424a291b06e80ddf6edd00f2e5f3fb4a
Mahider  Tekola <95926290+Mahider-T@users.noreply.github.com>
2024-04-02 15:46:09 +0300

    Update config.go

--- cmd/config.go
+++ cmd/config.go
@@ -5,5 +5,5 @@ import (
 )
 
 func SetEnv() {
-	os.Setenv("DATABASE_URL", "postgresql://postgres:Maverick2020!@localhost:5432/go-hexagon-task")
+	os.Setenv("DATABASE_URL", "postgresql://postgres:pass@localhost:5432/go-hexagon-task")
 }
